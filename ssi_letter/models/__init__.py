14de91a232fe52ddb55a1ac478347ae2cc689ad3
Andhitia Rama <andhitia.r@gmail.com>
2026-02-17 18:24:37 +0700

    [UPD]
    * Menambahkan internal memo

--- ssi_letter/models/__init__.py
+++ ssi_letter/models/__init__.py
@@ -2,10 +2,10 @@
 # Copyright 2023 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from . import (  # noqa: F401
-    letter_type,  # noqa: F401
-    internal_memo_type,  # noqa: F401
-    letter_mixin,  # noqa: F401
-    outgoing_letter,  # noqa: F401
-    incoming_letter,  # noqa: F401
-    internal_memo,  # noqa: F401
-)  # noqa: F401
+    letter_type,
+    internal_memo_type,
+    letter_mixin,
+    outgoing_letter,
+    incoming_letter,
+    internal_memo,
+)
