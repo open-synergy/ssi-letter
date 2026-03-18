0fb6d0a7f687005d4796f455cc95e8d3bb9459e6
Andhitia Rama <andhitia.r@gmail.com>
2026-03-18 21:30:14 +0700

    [ADD] ssi_letter_documenso_signing

---
+++ ssi_letter_documenso_signing/models/incoming_letter.py
@@ -0,0 +1,15 @@
+# Copyright 2026 OpenSynergy Indonesia
+# Copyright 2026 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+
+from odoo import models
+
+
+class IncomingLetter(models.Model):
+    _name = "incoming_letter"
+    _inherit = [
+        "incoming_letter",
+        "mixin.documenso_signing",
+    ]
+
+    _documenso_signing_create_page = True
---
+++ ssi_letter_documenso_signing/models/outgoing_letter.py
@@ -0,0 +1,15 @@
+# Copyright 2026 OpenSynergy Indonesia
+# Copyright 2026 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+
+from odoo import models
+
+
+class OutgoingLetter(models.Model):
+    _name = "outgoing_letter"
+    _inherit = [
+        "outgoing_letter",
+        "mixin.documenso_signing",
+    ]
+
+    _documenso_signing_create_page = True
